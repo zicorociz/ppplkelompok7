03275b120e2d1b32c18ac77c9c2082de313d4551
Zikrul Khalis <khaliszikrul16@gmail.com>
2025-07-03 16:49:23 +0700

    update

--- lib/views/admin/guest/add_guest_screen.dart
+++ lib/views/admin/guest/add_guest_screen.dart
@@ -4,18 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:get/get.dart';
-import 'package:stay_place/controller/admin/guest/add_guest_controller.dart';
-import 'package:stay_place/helpers/utils/my_shadow.dart';
-import 'package:stay_place/helpers/utils/ui_mixins.dart';
-import 'package:stay_place/helpers/widgets/my_breadcrumb.dart';
-import 'package:stay_place/helpers/widgets/my_breadcrumb_item.dart';
-import 'package:stay_place/helpers/widgets/my_card.dart';
-import 'package:stay_place/helpers/widgets/my_container.dart';
-import 'package:stay_place/helpers/widgets/my_spacing.dart';
-import 'package:stay_place/helpers/widgets/my_text.dart';
-import 'package:stay_place/helpers/widgets/my_text_style.dart';
-import 'package:stay_place/helpers/widgets/responsive.dart';
-import 'package:stay_place/views/layout/layout.dart';
+import 'package:sikilap/controller/admin/guest/add_guest_controller.dart';
+import 'package:sikilap/helpers/utils/my_shadow.dart';
+import 'package:sikilap/helpers/utils/ui_mixins.dart';
+import 'package:sikilap/helpers/widgets/my_breadcrumb.dart';
+import 'package:sikilap/helpers/widgets/my_breadcrumb_item.dart';
+import 'package:sikilap/helpers/widgets/my_card.dart';
+import 'package:sikilap/helpers/widgets/my_container.dart';
+import 'package:sikilap/helpers/widgets/my_spacing.dart';
+import 'package:sikilap/helpers/widgets/my_text.dart';
+import 'package:sikilap/helpers/widgets/my_text_style.dart';
+import 'package:sikilap/helpers/widgets/responsive.dart';
+import 'package:sikilap/views/layout/layout.dart';
 
 class AddGuestScreen extends StatefulWidget {
   const AddGuestScreen({super.key});
@@ -60,7 +60,8 @@ class _AddGuestScreenState extends State<AddGuestScreen> with UIMixin {
               Padding(
                 padding: MySpacing.x(flexSpacing),
                 child: MyCard(
-                  shadow: MyShadow(elevation: 0.2, position: MyShadowPosition.bottom),
+                  shadow: MyShadow(
+                      elevation: 0.2, position: MyShadowPosition.bottom),
                   paddingAll: 24,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -71,7 +72,8 @@ class _AddGuestScreenState extends State<AddGuestScreen> with UIMixin {
                       MySpacing.height(24),
                       commonTextField("Email", "Email Address"),
                       MySpacing.height(24),
-                      commonTextField("Contact Number", "Contact Number", numbered: true),
+                      commonTextField("Contact Number", "Contact Number",
+                          numbered: true),
                       MySpacing.height(24),
                       commonTextField("Email", "Email Address"),
                       MySpacing.height(24),
@@ -87,7 +89,8 @@ class _AddGuestScreenState extends State<AddGuestScreen> with UIMixin {
     );
   }
 
-  Widget commonTextField(String title, String hintText, {bool numbered = false}) {
+  Widget commonTextField(String title, String hintText,
+      {bool numbered = false}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -103,12 +106,18 @@ class _AddGuestScreenState extends State<AddGuestScreen> with UIMixin {
           decoration: InputDecoration(
             hintText: hintText,
             hintStyle: MyTextStyle.labelMedium(xMuted: true),
-            border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            errorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            focusedErrorBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            errorBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            disabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
             contentPadding: MySpacing.all(14),
             isCollapsed: true,
             isDense: true,
@@ -126,9 +135,11 @@ class _AddGuestScreenState extends State<AddGuestScreen> with UIMixin {
       onTap: controller.pickImage,
       child: controller.fileName != null
           ? controller.fileBytes != null
-              ? Image.memory(controller.fileBytes!, height: 200, width: 200, fit: BoxFit.cover)
+              ? Image.memory(controller.fileBytes!,
+                  height: 200, width: 200, fit: BoxFit.cover)
               : controller.filePath != null
-                  ? Image.file(File(controller.filePath!), fit: BoxFit.cover, height: 200, width: 200)
+                  ? Image.file(File(controller.filePath!),
+                      fit: BoxFit.cover, height: 200, width: 200)
                   : SizedBox()
           : Column(
               mainAxisAlignment: MainAxisAlignment.center,
